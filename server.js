f724d92a7fadb48d1e70c3207e1d49144e5feecf
Pavle <milosavljevic2007pavle1730@gmail.com>
2026-05-07 13:16:55 +0200

    Har installerat sqlite 3 och skrivit lite kod så att webbsidan kan fortfarande hålla data när programmet stängs, detta kod behövs skrivas klart

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const sqlite3 = require('sqlite3');
 
 const PORT = 3000;
+const db = new sqlite3.Database('./Projekt.db');
+
+db.run(`CREATE TABLE IF NOT EXISTS Test (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    namn TEXT NOT NULL, beskrivning TEXT,
+    status BOOLEAN NOT NULL,
+    prioritet TINYINT NOT NULL (prioritet BETWEEN 1 AND 3),
+    skapad DATE NOT NULL,
+    deadline DATE NOT NULL )`);
 
 app.get("/", (req,res) => {
-    res.send("Hello from my server!");
+    res.send("Servern funkar");
+});
+
+app.post("/NyttProjekt",(req,res) =>{
+    const id = req.params.id;
+    const namn = req.params.namn;
+    const status = req.params.status;
+    const prioritet = req.params.prioritet;
+    const skapad = req.params.skapad;
+    const deadline = req.params.deadline;
+
 });
 
 app.listen ( PORT, () => {
